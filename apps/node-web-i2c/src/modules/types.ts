4285e83034e389400a9f90842d4f05805bf68955
akihiko.kigure <akihiko.kigure@gmail.com>
2025-04-12 17:27:25 +0900

    feat: split code

---
+++ apps/node-web-i2c/src/modules/types.ts
@@ -0,0 +1,17 @@
+/** ポート番号 */
+export type PortNumber = number;
+/** ポート名 */
+export type PortName = string;
+
+/** I2C Slave アドレス */
+export type I2CSlaveAddress = number;
+
+/**
+ * I2C Port Map Max サイズ
+ */
+export const I2CPortMapSizeMax = 32;
+
+/**
+ * Uint16 Max サイズ
+ */
+export const Uint16Max = 65535;
