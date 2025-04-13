9370c572d9d81bb8311506e8007c7c7ca8d3442b
akihiko.kigure <akihiko.kigure@gmail.com>
2025-04-13 16:46:56 +0900

    refactor: replace parseUint16

--- libs/i2c/src/lib/types.ts
+++ libs/i2c/src/lib/types.ts
@@ -4,22 +4,7 @@
 export const I2CPortMapSizeMax = 32;
 
 /**
- * Uint16 Max サイズ
- */
-export const Uint16Max = 65535;
-
-/**
- * Uint16型変換処理
- * @param parseString 変換文字列
- * @return Uint16型変換値
- */
-export function parseUint16(parseString: string) {
-  const n = Number.parseInt(parseString, 10);
-  if (0 <= n && n <= Uint16Max) return n;
-  else throw new RangeError(`Must be between 0 and ${Uint16Max}.`);
-}
-
-/** I2C Slave アドレス */
+ * I2C Slave アドレス */
 export type I2CSlaveAddress = number;
 
 /** ポート番号 */
@@ -120,7 +105,7 @@ export class I2CPortMap extends Map<PortNumber, I2CPort> {
    */
   getByName(portName: PortName): I2CPort | undefined {
     const matches = /^i2c-(\d+)$/.exec(portName);
-    return matches == null ? undefined : this.get(parseUint16(matches[1]));
+    return matches == null ? undefined : this.get(parseInt(matches[1], 10));
   }
 }
 
