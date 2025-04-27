78c504ab9b901a4815b9f99aecc1e9b6ca58eae4
akihiko.kigure <akihiko.kigure@gmail.com>
2025-04-27 22:58:29 +0900

    feat: resplit index.ts for i2c

---
+++ node/i2c/src/i2c-port-map.ts
@@ -0,0 +1,10 @@
+import { I2CPort } from './i2c-port';
+import { PortName, PortNumber, parseUint16 } from './types';
+
+/** Different from Web I2C API specification. */
+export class I2CPortMap extends Map<PortNumber, I2CPort> {
+  getByName(portName: PortName): I2CPort | undefined {
+    const matches = /^i2c-(\d+)$/.exec(portName);
+    return matches == null ? undefined : this.get(parseUint16(matches[1]));
+  }
+}
--- node/i2c/src/request-i2c-access.ts
+++ node/i2c/src/request-i2c-access.ts
@@ -7,9 +7,3 @@ import { I2CAccess } from './i2c-access';
 export async function requestI2CAccess(): Promise<I2CAccess> {
   return new I2CAccess();
 }
-
-export { I2CAccess } from './i2c-access';
-export { I2CPort } from './i2c-port';
-export { I2CPortMap } from './i2c-port-map';
-export { I2CSlaveDevice } from './i2c-slave-device';
-export { OperationError } from './operation-error';
